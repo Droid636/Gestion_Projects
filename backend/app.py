41ca34555082420a737b3c0c3d8dd3eb0ba34f5d
Droid636 <2022043@utsh.edu.mx>
2025-07-01 16:48:40 -0600

    cambios

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from routes import api
+import os
 
-app = Flask(__name__)
+FRONTEND_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../frontend'))
+
+app = Flask(__name__, static_folder=FRONTEND_PATH)
 CORS(app)
 
 app.register_blueprint(api, url_prefix='/api')
 
+@app.route('/')
+def root():
+    return send_from_directory(FRONTEND_PATH, 'login.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(FRONTEND_PATH, path)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
